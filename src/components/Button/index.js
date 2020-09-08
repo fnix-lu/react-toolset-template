ec8aad36574ea40428db7522f08d115b59a36ae0
wenjun.lu <835913098@qq.com>
2020-09-08 11:32:10 +0800

    chore: add env files

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import styles from './index.module.scss'
 
 class Button extends Component {
+  componentDidMount () {
+    console.log(process.env.REACT_APP_BASE_API)
+  }
   render () {
     return <div className={styles.button}>Button</div>
   }
